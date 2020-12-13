1e41df28579fbdfde54da211df93de1a5024c5ef
arkapratimsarkar <contactarka2020@gmail.com>
2020-12-14 04:44:36 +0530

    updated product route & controller
    added funtionality of update, delete, stock & sold count updation, listing all products

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProductById, createProduct} = require('../controllers/product');
+const {
+    getProductById, 
+    createProduct, 
+    getProduct,
+    photo,
+    deleteProduct,
+    updateProduct,
+    getAllProducts,
+    getAllUniqueCategories
+} = require('../controllers/product');
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 const {getUserById} = require('../controllers/user');
 
@@ -9,7 +18,38 @@ const {getUserById} = require('../controllers/user');
 router.param('userId', getUserById);
 router.param('productId', getProductById);
 
-//all of actual routes 
-router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct)
+//all of actual routes
+//create route 
+router.post('/product/create/:userId', 
+isSignedIn, 
+isAuthenticated, 
+isAdmin, 
+createProduct
+);
+
+//read routes
+router.get('/product/:productId', getProduct);
+router.get('/product//photo/:productId', photo);
+
+//delete route
+router.delete('/product/:productId/:userId', 
+isSignedIn, 
+isAuthenticated, 
+isAdmin,
+deleteProduct
+);
+
+//update route
+router.delete('/product/:productId/:userId', 
+isSignedIn, 
+isAuthenticated, 
+isAdmin,
+updateProduct
+);
+
+//listing route
+router.get('/products', getAllProducts);
+
+router.get('/products/categories', getAllUniqueCategories);
 
 module.exports = router;
